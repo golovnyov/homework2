6fd119b7ec6d0aceb350e33103dcb146de4430a9
Slava <golovnjov@gmail.com>
2019-04-03 22:21:33 +0300

    Add project files.

---
+++ ConsoleApp1/CustomDataStructure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class CustomDataStructure
+    {
+        private Hashtable hashtable;
+
+        private object[] array;
+
+        public object this[int i]
+        {
+            set
+            {
+                SetValue(i, value);
+            }
+        }
+
+        private void SetValue(int index, object value)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Key.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Key
+    {
+        private Func<int> _hashFunction;
+
+        public Key(Func<int> hashFuncton)
+        {
+            _hashFunction = hashFuncton;
+        }
+
+        public override int GetHashCode()
+        {
+            return _hashFunction();
+        }
+    }
+}
